147e4d0b13c0b5a59706c9e0935a0863c3e0dc02
usamaraza21 <63732785+usamaraza21@users.noreply.github.com>
2021-08-16 18:19:26 +0500

    chapter1
    Q9. A lot of cell phones have tip calculators. Write one. Ask the user for the price of the meal and
    the percent tip they want to leave. Then print both the tip amount and the total bill with the
    tip included

---
+++ exercise9.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 16 17:26:40 2021
+
+@author: Usama
+"""
+
+"""
+9. A lot of cell phones have tip calculators. Write one. Ask the user for the price of the meal and
+the percent tip they want to leave. Then print both the tip amount and the total bill with the
+tip included
+"""
+bill=eval(input('enter the amount of meaL\n'))
+tip=eval(input('and the tip amount\n'))
+print('bill of meal is',bill,'and after tip the total is',bill+tip)
