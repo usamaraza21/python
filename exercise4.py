463df15de4b7238d371834a4ac641006650edf47
usamaraza21 <63732785+usamaraza21@users.noreply.github.com>
2021-08-16 18:13:27 +0500

    Add files via upload
    Write a program that computes and prints the result of 
    512 − 282/47·48 + 5.

---
+++ exercise4.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 16 16:13:01 2021
+
+@author: Usama
+"""
+"""
+Write a program that computes and prints the result of 
+512 − 282/47·48 + 5.
+ It is roughly .1017.
+"""
+print('the given details are written as ',(512-282)/(47.48+5))
