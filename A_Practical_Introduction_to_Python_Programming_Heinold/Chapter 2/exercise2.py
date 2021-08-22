7685cdb018eb800e1e885dc2c3589188dcce58ec
usamaraza21 <63732785+usamaraza21@users.noreply.github.com>
2021-08-22 22:41:40 +0500

    Add files via upload
    Q2. Write a program to fill the screen horizontally and vertically with your name. [Hint: add the
    option end='' into the print function to fill the screen horizontally.]

---
+++ A_Practical_Introduction_to_Python_Programming_Heinold/Chapter 2/exercise2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 17 02:13:55 2021
+
+@author: Usama
+"""
+
+"""
+Q2. Write a program to fill the screen horizontally and vertically with your name. [Hint: add the
+option end='' into the print function to fill the screen horizontally.]
+"""
+
+for i in range(10):
+    print('t1ger',end='')
+for j in range(10):
+    print('t1ger')
