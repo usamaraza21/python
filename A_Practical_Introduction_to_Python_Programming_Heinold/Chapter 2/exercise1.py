fbdad8db56d8b7ee9fd90437f6afc944a5141168
usamaraza21 <63732785+usamaraza21@users.noreply.github.com>
2021-08-22 22:41:00 +0500

    Add files via upload
    Q1. Write a program that prints your name 100 times.

---
+++ A_Practical_Introduction_to_Python_Programming_Heinold/Chapter 2/exercise1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 17 02:10:07 2021
+
+@author: Usama
+"""
+
+"""
+Q1. Write a program that prints your name 100 times.
+"""
+
+for i in range(100):
+    print(i,'wounded t1ger')
+
+
+#another way
+
+for i in range(1,101):
+    print(i,'wounded t1ger')
