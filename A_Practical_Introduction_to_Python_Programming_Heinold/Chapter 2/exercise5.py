43984801c3ce46d5568028477740ff607b405456
usamaraza21 <63732785+usamaraza21@users.noreply.github.com>
2021-08-22 22:43:41 +0500

    Add files via upload
    Q5.Write a program that uses a for loop to print the numbers 8, 11, 14, 17, 20, . . . , 83, 86, 89

---
+++ A_Practical_Introduction_to_Python_Programming_Heinold/Chapter 2/exercise5.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 20 09:42:09 2021
+
+@author: Usama
+"""
+
+"""
+Q5.Write a program that uses a for loop to print the numbers 8, 11, 14, 17, 20, . . . , 83, 86, 89
+"""
+for i in range(8,90,3):
+    print(i)
