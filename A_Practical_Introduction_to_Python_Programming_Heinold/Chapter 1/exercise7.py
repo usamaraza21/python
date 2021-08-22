9e740df6a4e2f8660845fa5f3bf35c0575df486a
usamaraza21 <63732785+usamaraza21@users.noreply.github.com>
2021-08-22 22:07:59 +0500

    Add files via upload
    7. Write a program that asks the user for a weight in kilograms and converts it to pounds. There
    are 2.2 pounds in a kilogram.

---
+++ A_Practical_Introduction_to_Python_Programming_Heinold/Chapter 1/exercise7.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 16 16:37:18 2021
+
+@author: Usama
+"""
+
+"""
+7. Write a program that asks the user for a weight in kilograms and converts it to pounds. There
+are 2.2 pounds in a kilogram.
+"""
+
+weight=eval(input('enter your weight in kg\n'))
+print('your weight in pounds is:',weight*2.2,'pounds')
