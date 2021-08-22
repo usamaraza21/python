da8af781394aa5b550bb198a178a4a6d6e86444a
usamaraza21 <63732785+usamaraza21@users.noreply.github.com>
2021-08-22 22:42:20 +0500

    Add files via upload
    Q3. Write a program that outputs 100 lines, numbered 1 to 100, each with your name on it. The
    output should look like the output below.
    1 Your name
    2 Your name
    3 Your name
    4 Your name
    ...
    100 Your name

---
+++ A_Practical_Introduction_to_Python_Programming_Heinold/Chapter 2/exercise3.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 17 02:20:30 2021
+
+@author: Usama
+"""
+
+"""
+Q3. Write a program that outputs 100 lines, numbered 1 to 100, each with your name on it. The
+output should look like the output below.
+1 Your name
+2 Your name
+3 Your name
+4 Your name
+...
+100 Your name
+"""
+
+for i in range(1,101):
+    print(i,'t1ger')
